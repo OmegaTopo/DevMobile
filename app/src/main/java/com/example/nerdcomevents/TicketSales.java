0acd61a15045cfa3b500c89697e07e4dec10185a
unknown <lucassbettio@gmail.com>
2023-11-21 20:53:26 -0300

    Ticket Sales Screen

---
+++ app/src/main/java/com/example/nerdcomevents/TicketSales.java
@@ -0,0 +1,14 @@
+package com.example.nerdcomevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketSales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ticket_sales);
+    }
+}
